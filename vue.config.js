5d728dfc03524c24e5c654b605baa23a9a7ff92f
zhz <azhz1107cat@outlook.com>
2025-08-15 13:42:05 +0800

    developing...

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
-
 const repoName = 'zata-website'
 
 module.exports = defineConfig({
   transpileDependencies: true,
   
   publicPath: process.env.NODE_ENV === 'production' 
-    ? `/${repoName}/`
-    : '/',
+    ? // 域名下用根路径 "/"
+      (typeof window !== 'undefined' && window.location.host === 'zata.random321.com')
+        ? '/' 
+        : `/${repoName}/` // GitHub 默认地址下用仓库名路径
+    : '/', // 开发环境保持根路径
   
-  outputDir: 'dist',
-  
-  assetsDir: 'assets',
-  
+ 
   chainWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
       config.plugin('copy').use(require('copy-webpack-plugin'), [
@@ -21,7 +20,8 @@ module.exports = defineConfig({
             {
               from: 'CNAME',
               to: 'CNAME',
-              toType: 'file'
+              toType: 'file',
+              noErrorOnMissing: true
             }
           ]
         }
